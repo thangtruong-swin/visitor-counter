ea33fb8067ea5714521c0714f0d2c3d4867540e7
Thang Truong <thang.truong.swin@gmail.com>
2022-10-05 17:07:47 +1100

    week1 done

---
+++ index.js
@@ -0,0 +1,27 @@
+let countEL = document.getElementById("counter-el");
+let saveEL = document.getElementById("save-el");
+
+// console.log(countEL);
+let count = 0;
+function increment() {
+	// console.log("Button was clicked");
+	count += 1;
+	countEL.textContent = count;
+}
+
+function Decrement() {
+	count -= 1;
+	countEL.innerText = count;
+}
+
+function save() {
+	// console.log(count);
+	let countStr = count + " - ";
+	saveEL.textContent += countStr;
+	countEL.textContent = 0;
+	count = 0;
+}
+
+function reset() {
+	saveEL.textContent = "The previous entered: ";
+}
